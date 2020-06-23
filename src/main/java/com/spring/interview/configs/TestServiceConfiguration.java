02d82fbc75f06d5d689e29bed6748810fdaf0378
jianzhong.rong <tony.tiger.rong@gmail.com>
2020-06-23 20:39:11 -0300

    @profile @activeprofiles @SpringBootTest @ContextConfiguation(class= applicationconfiguration.class) // load configure class @runwith( springjunit4classrunner.class)

---
+++ src/main/java/com/spring/interview/configs/TestServiceConfiguration.java
@@ -0,0 +1,27 @@
+package com.spring.interview.configs;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+import com.spring.interview.dao.BaseDao;
+import com.spring.interview.services.TestService;
+import org.mockito.Mockito;
+
+@Profile("teststart")
+@SpringBootConfiguration
+public class TestServiceConfiguration {
+
+	@Bean
+	@Primary
+	public TestService testService() {
+		return Mockito.mock(TestService.class);
+	}
+	
+	@Bean
+	public BaseDao dao() {
+		return Mockito.mock(BaseDao.class);
+	}
+}
---
+++ src/test/java/com/spring/interview/services/TestServiceTest2.java
@@ -0,0 +1,45 @@
+package com.spring.interview.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.spring.interview.SpringInterviewQaApplication;
+import com.spring.interview.configs.TestServiceConfiguration;
+
+@ActiveProfiles("teststart")
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = TestServiceConfiguration.class)
+class TestServiceTest2 {
+
+	@Autowired
+	TestService testService;
+	
+	@Test
+	void test() {
+		List<String> mockStrList = Arrays.asList("test1", "test2", "test3");
+		// when we invoke dao.getTestData(), then we get a mocked return result
+		when(testService.getTestData("")).thenReturn(mockStrList);
+		// instance of testService invokes dao which is mocked
+		List<String> result = testService.getTestData("");
+		for (String str : result) {
+			System.out.println(str);
+		}
+		assertTrue(result.get(0).equals("test1"));
+		assertTrue(result.get(1).equals("test2"));
+		assertTrue(result.get(2).equals("test3")); 
+	}
+
+}
