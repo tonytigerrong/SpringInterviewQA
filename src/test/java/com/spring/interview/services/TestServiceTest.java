63eecbda3e2a8c27304d21b51e41f0918434b175
jianzhong.rong <tony.tiger.rong@gmail.com>
2020-06-23 19:46:17 -0300

    mocktest comments

--- src/test/java/com/spring/interview/services/TestServiceTest.java
+++ src/test/java/com/spring/interview/services/TestServiceTest.java
@@ -24,23 +24,29 @@ import com.spring.interview.dao.BaseDao;
 class TestServiceTest {
 
 	@Mock
-	BaseDao dao;
-	
+	BaseDao dao; // need mock in init function, for isolation servcie and test
+
 	@InjectMocks
-	TestService testService;
-	
+	TestService testService; // inject mocks is instance which invoke a mocked instance
+
 	@Before
-	public  void init() {
+	public void init() {
 		MockitoAnnotations.initMocks(this);
 	}
+
 	@Test
 	void getTestDataTest() {
-		List<String> mockStrList = Arrays.asList("test1","test2","test3");
+		List<String> mockStrList = Arrays.asList("test1", "test2", "test3");
+		// when we invoke dao.getTestData(), then we get a mocked return result
 		when(dao.getTestData()).thenReturn(mockStrList);
+		// instance of testService invokes dao which is mocked
 		List<String> result = testService.getTestData("");
 		for (String str : result) {
 			System.out.println(str);
 		}
+		assertTrue(result.get(0).equals("test1"));
+		assertTrue(result.get(1).equals("test2"));
+		assertTrue(result.get(2).equals("test3"));
 	}
 
 }
